92e0b0f6b7d0d35bf2fe116106f3e799a048e3cc
Leo <111384016+hashketchum208@users.noreply.github.com>
2023-04-03 20:07:42 -0600

    made changes to Handlebars and JS

---
+++ Develop/controllers/api/postRoutes.js
@@ -0,0 +1,91 @@
+const router = require('express').Router();
+const { Post, User } = require('../../models');
+
+// GET all posts
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [{ model: User, attributes: ['userName'] }]
+    });
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET a post by ID
+router.get('/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [{ model: User, attributes: ['userName'] }]
+    });
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// CREATE a new post
+router.post('/', async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      title: req.body.title,
+      post_content: req.body.content,
+      user_id: req.session.user_id,
+    });
+    res.status(201).json(newPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// UPDATE a post by ID
+router.put('/:id', async (req, res) => {
+  console.log("edit this post!");
+  try {
+    console.log("there you go!");
+    const updatedPost = await Post.update(
+      {
+        title: req.body.title,
+        post_content: req.body.content,
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    );
+    if (!updatedPost[0]) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+    res.status(200).json(updatedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE a post by ID
+router.delete('/:id', async (req, res) => {
+  console.log("delete!");
+  try {
+    console.log("its deleted")
+    const deletedPost = await Post.destroy({
+        where: {
+        id: req.params.id}
+    });
+    res.status(201).json(deletedPost);
+    if (!deletedPost) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
---
+++ Develop/public/js/home.js
@@ -0,0 +1,48 @@
+const newPostHandler = async (event) => {
+  event.preventDefault();
+
+  // Collect values from new post form
+  const title = document.querySelector("#new-post-title").value.trim();
+  const content = document.querySelector("#new-post-content").value.trim();
+
+  // Send a POST request to the API endpoint
+  const response = await fetch(`/api/post`, {
+    method: "POST",
+    body: JSON.stringify({ title, content }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    // If successful, redirect browser to new post page
+    document.location.replace("/");
+  } else {
+    alert("Cannot add post");
+  }
+};
+
+
+const deleteFormHandler = async (event) => {
+  event.preventDefault();
+  console.log("here again");
+
+  const id = event.target.getAttribute("data-id");
+  console.log(id);
+  const response = await fetch(`/api/post/${id}`, {
+    method: "DELETE",
+  });
+
+  if (response.ok) {
+  document.location.replace("/");
+  } else {
+    alert(response.statusText);
+  }
+}
+
+const deleteButtons = document.querySelectorAll(".delete-btn");
+deleteButtons.forEach((button) => {
+  button.addEventListener("click", deleteFormHandler);
+});
+
+document
+  .querySelector("#new-post-form")
+  .addEventListener("submit", newPostHandler);
